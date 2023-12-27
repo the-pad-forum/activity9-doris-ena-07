2fc64148d0e031e466289979eaf66b08cbb035a1
Doris Ena <dorisenak77@gmail.com>
2023-12-27 18:27:51 +0000

    refactor Navbar and update global css

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,43 +8,21 @@ interface NavbarProps {
 }; 
 
 const Navbar:React.FC<NavbarProps> = ({view, setView}) => {
-  const btnClasses='cursor-pointer transition-all duration-300 rounded-md py-1.5 px-3.5 text-indigo-900'
-
-  const onMouseEnter = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const element = e.target as HTMLButtonElement;
-
-    if (!element.classList.contains('bg-indigo-400')) {
-      element.classList.add('text-indigo-400')
-    }
-  }
-
-  const onMouseLeave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const element = e.target as HTMLButtonElement;
-
-    element.classList.remove('text-indigo-400')
-  }
-
   return (
     <div className="bg-white opacity-2 shadow-md flex flex-row items-center gap-x-3 justify-end fixed h-[80px] w-[100%] left-0 top-0 z-10 px-6">
       <button 
-        className={`${btnClasses} ${view === 'Albums' && 'bg-indigo-400'}`} 
+        className={`nav-item ${view === 'Albums' && 'active'}`} 
         onClick={()=>setView('Albums')}
-        onMouseEnter={(e) => onMouseEnter(e)}
-        onMouseLeave={(e) => onMouseLeave(e)}
       >Albums</button>
 
       <button 
-        className={`${btnClasses} ${view === 'Users' && 'bg-indigo-400'}`} 
+        className={`nav-item ${view === 'Users' && 'active'}`} 
         onClick={()=>setView('Users')}
-        onMouseEnter={(e) => onMouseEnter(e)}
-        onMouseLeave={(e) => onMouseLeave(e)}
       >Users</button>
 
       <button 
-        className={`${btnClasses} ${view === 'Posts' && 'bg-indigo-400'}`} 
+        className={`nav-item ${view === 'Posts' && 'active'}`} 
         onClick={()=>setView('Posts')}
-        onMouseEnter={(e) => onMouseEnter(e)}
-        onMouseLeave={(e) => onMouseLeave(e)}
       >Posts</button>
     </div>
   );
