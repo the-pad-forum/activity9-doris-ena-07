cfc5b068462dd46a82509f6aca8891428e03a0be
Doris Ena <dorisenak77@gmail.com>
2023-12-29 21:49:14 +0000

    reposition table

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 "use client"
+import React from 'react'
 import Albums from './components/Albums/Albums'
 import { useState } from 'react'
-// import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer'
 
 export default function Home() {
   return (
-    <div className="container mx-auto flex flex-col space-y-7 pt-2 mt-4]">
+    <div className="container mx-auto  flex flex-col space-y-7 pt-2 mt-4]">
       <h2 className='text-white font-bold text-center text-[50px]'>Albums</h2>
 
       <Albums />
     </div>
+
+    // <Footer/>
   )
 
-  // <Footer/>
+  
 }
