efaf29de0b51dbb5580ebcaacbec1b156b5a01cf
Doris Ena <dorisenak77@gmail.com>
2023-12-29 23:11:09 +0000

    add margin

--- app/components/Posts/Posts.tsx
+++ app/components/Posts/Posts.tsx
@@ -64,10 +64,10 @@ const Posts = () => {
 
       </div>
 
-      <div className='flex flex-row items-center gap-4 text-black mt-5 justify-end pb-2'>
-        <button className= {` bg-indigo-500 px-4 py-1 border-indigo-900 border-solid border-[1px] ${currentPage === 1? 'text-black/30' :'text-white' }`} onClick={handlePrevPage}>Prev.</button>
+      <div className='flex flex-row items-center pb-8 gap-4 text-black mt-5 justify-end'>
+        <button className= {` bg-indigo-500 px-4 py-1 border-indigo-400 border-solid border-[1px] rounded-md ${currentPage === 1? 'text-black/30' :'text-white' }`} onClick={handlePrevPage}>Prev.</button>
         <p>{currentPage}</p>
-        <button className={` bg-indigo-500 px-2 py-1 border-indigo-900 border-solid border-[1px] ${currentPage === totalPages? 'text-black/30' :'text-white'}`} onClick={handleNextPage}>Next</button>
+        <button className={` bg-indigo-500 px-2 py-1 border-indigo-400 border-solid border-[1px] rounded-md ${currentPage === totalPages? 'text-black/30' :'text-white'}`} onClick={handleNextPage}>Next</button>
       </div>
     </>
   );
