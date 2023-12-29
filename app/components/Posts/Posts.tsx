780a1e3363d908435c683a83582f604e5cbcf007
Doris Ena <dorisenak77@gmail.com>
2023-12-29 20:36:22 +0000

    reposition pagination nav button

--- app/components/Posts/Posts.tsx
+++ app/components/Posts/Posts.tsx
@@ -10,22 +10,12 @@ interface Post {
   body: string;
 }
 
-interface PostsProps {
-  view: string; // Change 'string' to the actual type of your 'view' prop
-  setView: React.Dispatch<React.SetStateAction<string>>}; // Change 'string' to the actual type of your 'view' prop
-
-  const Posts:React.FC<PostsProps> = ({view, setView}) =>{
+const Posts = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [currentPage, setCurrentPage]=useState<number>(1)
-  const [showPage, setShowpage] = useState<boolean>(false)
-
-
-
-  function capitalizeFirstCharacter(str: string){
-    if (str.length===0){return ''}
-    const first=str[0].toUpperCase()
-    return str.replace(str.charAt(0),first)
-  }
+  const itemsPerPage = 12;
+  const totalPages = Math.ceil(posts.length / itemsPerPage);
+  const visibleData = posts.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -33,47 +23,54 @@ interface PostsProps {
       .then(data => setPosts(data as Post[]));
   }, []);
 
-const itemsPerPage = 12;
-const totalPages = Math.ceil(posts.length / itemsPerPage);
-
-const visibleData = posts.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
-
-const handleNextPage = () => {
-  if (currentPage < totalPages) {
-    setCurrentPage(currentPage + 1);
+  function capitalizeFirstCharacter(str: string){
+    if (str.length===0){return ''}
+    const first=str[0].toUpperCase()
+    return str.replace(str.charAt(0),first)
   }
-};
 
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-
-const handlePrevPage = () => {
-  if (currentPage > 1) {
-    setCurrentPage(currentPage - 1);
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+    const visiblePages = 1; // Adjust the number of vi
   }
-};
-const getPageNumbers = () => {
-  const pageNumbers = [];
-  const visiblePages = 1; // Adjust the number of vi
-}
 
-return (
-  <>
-  <div className='flex flex-col mt-5 space-y-8'>
-    {visibleData.map(post => (
-      <div key={post.id}>
-        <h3 className='text-slate-800 text-2xl font-semibold capitalize'>{post.title}.</h3>
-        <p className='text-slate-800'>{capitalizeFirstCharacter(post.body)}</p>
+  return (
+    <>
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 py-10 pt-2'>
+        {visibleData.map(post => (
+          <div key={post.id}>
+            <div className="card bg-white shadow-xl h-[300px]">
+              <div className="card-body">
+                <h3 className='text-slate-800 text-2xl font-semibold capitalize'>{post.title}. </h3>
+
+                <p className='text-slate-800'>{capitalizeFirstCharacter(post.body)}</p>
+              </div>
+            </div>
+            {/* <h3 className='text-slate-800 text-2xl font-semibold capitalize'>{post.title}.</h3>
+            <p className='text-slate-800'>{capitalizeFirstCharacter(post.body)}</p> */}
+          </div>
+        ))}
+
       </div>
-    ))}
 
-  </div>
-   <div className='flex flex-row items-center gap-4 text-black mt-5'>
-   <button className= {` bg-indigo-500 px-2 py-1 border-indigo-900 border-solid border-[1px] ${currentPage === 1? 'text-black/30' :'text-white' }`} onClick={handlePrevPage}>Prev.</button>
-   <p>{currentPage}</p>
-   <button className={` bg-indigo-500 px-2 py-1 border-indigo-900 border-solid border-[1px] ${currentPage === totalPages? 'text-black/30' :'text-white'}`} onClick={handleNextPage}>Next</button>
- </div>
- </>
-);
+      <div className='flex flex-row items-center gap-4 text-black mt-5 justify-end pb-2'>
+        <button className= {` bg-indigo-500 px-4 py-1 border-indigo-900 border-solid border-[1px] ${currentPage === 1? 'text-black/30' :'text-white' }`} onClick={handlePrevPage}>Prev.</button>
+        <p>{currentPage}</p>
+        <button className={` bg-indigo-500 px-2 py-1 border-indigo-900 border-solid border-[1px] ${currentPage === totalPages? 'text-black/30' :'text-white'}`} onClick={handleNextPage}>Next</button>
+      </div>
+    </>
+  );
   
 };
 
