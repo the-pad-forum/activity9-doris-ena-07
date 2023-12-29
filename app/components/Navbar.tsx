7fdc4f30325cd3b34c4cdac2bae026db445c2676
Doris Ena <dorisenak77@gmail.com>
2023-12-29 23:10:17 +0000

    style navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,10 +12,10 @@ const Navbar = () => {
     <div className="bg-white opacity-2 shadow-md flex flex-row justify-between items-center gap-x-3 fixed h-[80px] w-full left-0 top-0 z-10 px-6">
       <div className="flex items-center space-x-2">
         <Image src="/logo.png" height={100} width={100} alt="logo"/>
-        <p className="text-indigo-900 text-[25px]">Fetch API Project</p> 
+        <p className="text-indigo-900 text-[25px] font-bold">Fetch API Project</p> 
       </div>
 
-      <div className="flex flex-row items-center gap-x-3">
+      <div className="flex flex-row items-center gap-x-3 font-bold">
         <Link href="/" className={`nav-item ${pathname === '/' && 'active'}`}>Albums</Link>
 
         <Link href="/users" className={`nav-item ${pathname === '/users' && 'active'}`}>Users</Link>
