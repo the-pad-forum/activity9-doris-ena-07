ecedd2c71987a778e860da4ae867165c72878044
Doris Ena <dorisenak77@gmail.com>
2023-12-29 23:11:49 +0000

    modify layout

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,7 +2,7 @@ import Users from "../components/Users/Users";
 
 export default function UsersPage() {
     return (
-        <div className="container mx-auto flex flex-col space-y-7">
+        <div className="container mx-auto flex flex-col space-y-7  mt-2 mb-8">
             <h2 className='text-white font-bold text-center text-[50px]'>Users</h2>
 
             <Users />
